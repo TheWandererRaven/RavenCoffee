eab994384697c529f607d593ab4efad65c405a0c
TheWandererRaven <gabrielcorderor@gmail.com>
2025-10-21 14:45:18 -0600

    Added reciever in Fabric (I forgot on previous commit)

--- fabric/src/main/java/com/thewandererraven/ravencoffee/RavenCoffeeFabricClient.java
+++ fabric/src/main/java/com/thewandererraven/ravencoffee/RavenCoffeeFabricClient.java
@@ -1,25 +1,48 @@
 package com.thewandererraven.ravencoffee;
 
+import com.mojang.authlib.minecraft.client.MinecraftClient;
+import com.thewandererraven.ravencoffee.effect.breweffect.MultiEffectInstance;
+import com.thewandererraven.ravencoffee.effect.breweffect.MultiEffectsRegistry;
 import com.thewandererraven.ravencoffee.menu.MenusRegistry;
-import com.thewandererraven.ravencoffee.platform.Services;
-import com.thewandererraven.ravencoffee.platform.services.IMenuFactory;
-import com.thewandererraven.ravencoffee.platform.services.IScreenFactory;
+import com.thewandererraven.ravencoffee.networking.SyncBrewPayload;
+import com.thewandererraven.ravencoffee.platform.services.IBrewManagerHolder;
 import com.thewandererraven.ravencoffee.screen.CoffeeGrinderScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.MenuAccess;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.MenuType;
-
-import java.util.Map;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.player.Player;
 
 public class RavenCoffeeFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         MenuScreens.register(MenusRegistry.COFFEE_GRINDER.get(), CoffeeGrinderScreen::new);
-    }
+        ClientPlayNetworking.registerGlobalReceiver(
+                SyncBrewPayload.TYPE,
+                (payload, context) -> {
+                    Minecraft client = Minecraft.getInstance();
+                    client.execute(() -> {
+                        Player player = client.player;
+                        IBrewManagerHolder holder = (IBrewManagerHolder) player;
+                        if(payload.brewId() != null) {
+                        var holderEffect = MultiEffectsRegistry.BREW_EFFECTS.getEntries().stream()
+                                .filter(entry -> entry.get().getId().equals(payload.brewId()))
+                                .findFirst().orElse(null);
+                        if (holderEffect == null) return;
 
+                        MultiEffectInstance instance = new MultiEffectInstance(holderEffect.asHolder());
+                        holder.ravencoffee$getBrewEffectManager()
+                                .setClientEffect(instance, payload.duration());
+                        }
+                    });
+                }
+        );
+//        HudRenderCallback.EVENT.register((guiGraphics, listener) -> {
+//            Minecraft minecraft = Minecraft.getInstance();
+//            if(minecraft.player == null) return;
+//            g
+//        });
+    }
 }
